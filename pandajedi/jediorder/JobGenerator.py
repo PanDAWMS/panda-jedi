150f59c9ab1963999cef6305f06fee70086f4bf3
Fernando Barreiro Megino <fh.barreiro@gmail.com>
2016-07-05 10:12:14 +0200

    Propagate share from task to job in jobGenerator

--- pandajedi/jediorder/JobGenerator.py
+++ pandajedi/jediorder/JobGenerator.py
@@ -828,6 +828,7 @@ class JobGeneratorThread (WorkerThread):
                     jobSpec.currentPriority  = jobSpec.assignedPriority
                     jobSpec.lockedby         = 'jedi'
                     jobSpec.workQueue_ID     = taskSpec.workQueue_ID
+                    jobSpec.share     = taskSpec.share
                     # disable reassign
                     if taskSpec.disableReassign():
                         jobSpec.relocationFlag = 2
