05223a194ab843d6fe2ab21621b08d19f181261f
tmaeno <tmaeno@bnl.gov>
2016-10-06 15:12:15 +0200

    * added addEsParams

--- pandajedi/jedirefine/AtlasProdTaskRefiner.py
+++ pandajedi/jedirefine/AtlasProdTaskRefiner.py
@@ -1,5 +1,6 @@
 import re
 import sys
+import shlex
 
 from pandajedi.jedicore import Interaction
 from TaskRefinerBase import TaskRefinerBase
@@ -31,6 +32,40 @@ class AtlasProdTaskRefiner (TaskRefinerBase):
         tmpLog = self.tmpLog
         tmpLog.debug('start taskType={0}'.format(self.taskSpec.taskType))
         try:
+            # add ES paramsters
+            if 'addEsParams' in taskParamMap and taskParamMap['addEsParams'] == True:
+                preInclude = False
+                preExec = False
+                for tmpItem in taskParamMap['jobParameters']:
+                    if 'value' in tmpItem:
+                        if 'preInclude' in tmpItem['value']:
+                            tmpStr = '<PANDA_ES_ONLY>,AthenaMP/AthenaMP_EventService.py</PANDA_ES_ONLY>'
+                            tmpItem['value'] = self.insertString('preInclude',tmpStr,tmpItem['value'])
+                            preInclude = True
+                        if 'preExec' in tmpItem['value']:
+                            tmpStr  = '<PANDA_ES_ONLY>;'
+                            tmpStr += 'import os;pilot_tmp=type(str(),(),{})();'
+                            tmpStr += 'pilot_tmp.__dict__.update(**os.environ);'
+                            tmpStr += 'from AthenaMP.AthenaMPFlags import jobproperties as jps;'
+                            tmpStr += 'jps.AthenaMPFlags.EventRangeChannel=pilot_tmp.PILOT_EVENTRANGECHANNEL'
+                            tmpStr += '</PANDA_ES_ONLY>'
+                            tmpItem['value'] = self.insertString('preExec',tmpStr,tmpItem['value'])
+                            preExec = True
+                # add if missing
+                if not preInclude:
+                    tmpStr = '<PANDA_ES_ONLY>preInclude="AthenaMP/AthenaMP_EventService.py"</PANDA_ES_ONLY>'
+                    taskParamMap['jobParameters'].append({'type':'constant',
+                                                          'value':tmpStr})
+                if not preExec:
+                    tmpStr  = '<PANDA_ES_ONLY>preExec="'
+                    tmpStr += 'import os;pilot_tmp=type(str(),(),{})();'
+                    tmpStr += 'pilot_tmp.__dict__.update(**os.environ);'
+                    tmpStr += 'from AthenaMP.AthenaMPFlags import jobproperties as jps;'
+                    tmpStr += 'jps.AthenaMPFlags.EventRangeChannel=pilot_tmp.PILOT_EVENTRANGECHANNEL'
+                    tmpStr += '"</PANDA_ES_ONLY>'
+                    taskParamMap['jobParameters'].append({'type':'constant',
+                                                          'value':tmpStr})
+            # basic refine    
             self.doBasicRefine(taskParamMap)
             # set nosplit+repeat for DBR
             for datasetSpec in self.inSecDatasetSpecList:
@@ -89,4 +124,20 @@ class AtlasProdTaskRefiner (TaskRefinerBase):
         tmpLog.debug('done')
         return self.SC_SUCCEEDED
             
-    
+
+
+    # insert string
+    def insertString(self,paramName,tmpStr,origStr):
+        items = shlex.split(origStr,posix=False)
+        newStr = ''
+        for item in items:
+            if not paramName in item:
+                newStr += item
+            else:
+                newStr += item[:-1]
+                newStr += tmpStr
+                newStr += item[-1]
+            newStr += ' '
+        return newStr
+
+
