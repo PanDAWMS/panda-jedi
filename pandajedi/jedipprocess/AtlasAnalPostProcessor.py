2c9ec72e501ff7e7ae9a5d222a8b88693594147d
tmaeno <tmaeno@bnl.gov>
2017-03-27 15:45:45 +0200

    * changed AtlasAPP to extend lifetime of user output datasets

--- pandajedi/jedipprocess/AtlasAnalPostProcessor.py
+++ pandajedi/jedipprocess/AtlasAnalPostProcessor.py
@@ -94,6 +94,11 @@ class AtlasAnalPostProcessor (PostProcessorBase):
                         emptyOnly = False
                     retStr = ddmIF.deleteDataset(datasetSpec.datasetName,emptyOnly,ignoreUnknown=True)
                     tmpLog.debug(retStr)
+                # extend lifetime
+                if datasetSpec.type in ['output'] and datasetSpec.datasetName.startswith('user'):
+                    tmpLog.debug('extend lifetime datasetID={0}:Name={1}'.format(datasetSpec.datasetID,datasetSpec.datasetName))
+                    ddmIF.updateReplicationRules(datasetSpec.datasetName,{'type=.+':{'lifetime':7*24*60*60},
+                                                                          '(SCRATCH|USER)DISK':{'lifetime':14*24*60*60}})
                 # update dataset in DB
                 self.taskBufferIF.updateDatasetAttributes_JEDI(datasetSpec.jediTaskID,datasetSpec.datasetID,
                                                                {'state':datasetSpec.state,
