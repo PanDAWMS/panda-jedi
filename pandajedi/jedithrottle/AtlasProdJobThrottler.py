d05cbde75693b365d5e33f48eee4b5e6ec1356f4
Fernando Barreiro Megino <fh.barreiro@gmail.com>
2017-07-10 22:12:21 +0200

    Added vo parameter to getConfiguration call

--- pandajedi/jedithrottle/AtlasProdJobThrottler.py
+++ pandajedi/jedithrottle/AtlasProdJobThrottler.py
@@ -107,7 +107,7 @@ class AtlasProdJobThrottler (JobThrottlerBase):
         tmpLog.debug(msgHeader+' start workQueueID={0}'.format(workQueueID))
 
         # get central configuration values
-        configQueueLimit, configQueueCap, configRunningCap = self.__getConfiguration(workQueue.queue_name, resource_name)
+        configQueueLimit, configQueueCap, configRunningCap = self.__getConfiguration(vo, workQueue.queue_name, resource_name)
         tmpLog.debug(msgHeader + ' got configuration configQueueLimit={0}, configQueueCap={1}, configRunningCap={2}'
                      .format(configQueueLimit, configQueueCap, configRunningCap))
 
