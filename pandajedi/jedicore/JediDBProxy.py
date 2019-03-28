5344a6312210866e925fe36785bdf2c5a512ad31
fbarreir <fh.barreiro@gmail.com>
2019-03-28 13:44:01 +0100

    typo

--- pandajedi/jedicore/JediDBProxy.py
+++ pandajedi/jedicore/JediDBProxy.py
@@ -5060,7 +5060,7 @@ class DBProxy(taskbuffer.OraDBProxy.DBProxy):
                     varMap[':newDatasetID'] = self.cur.var(cx_Oracle.NUMBER)            
                     # insert dataset
                     self.cur.execute(sql+comment,varMap)
-                    val = self.getvalue_corrector(varMap[':newDatasetID'].getvalue()
+                    val = self.getvalue_corrector(varMap[':newDatasetID'].getvalue())
                     datasetID = long(val)
                     datasetIdMap[datasetSpec.outputMapKey()] = datasetID
                     datasetSpec.datasetID = datasetID
