581c59e6010137a41181d7bc15bbe67aa9705b78
tmaeno <tmaeno@bnl.gov>
2020-05-20 13:58:48 +0200

    fix

--- pandajedi/jeditest/addHpoTask.py
+++ pandajedi/jeditest/addHpoTask.py
@@ -1,4 +1,8 @@
 import uuid
+try:
+    from urllib.parse import quote
+except ImportError:
+    from urllib import quote
 
 from userinterface import Client
 
@@ -14,9 +18,9 @@ taskParamMap['taskPriority'] = 1000
 taskParamMap['reqID'] = 12345
 taskParamMap['architecture'] = 'power9'
 taskParamMap['hpoWorkflow'] = True
-taskParamMap['transUses'] = 'some_A'
-taskParamMap['transHome'] = 'some_B'
-taskParamMap['transPath'] = 'training.py'
+taskParamMap['transUses'] = ''
+taskParamMap['transHome'] = ''
+taskParamMap['transPath'] = 'https://pandaserver.cern.ch:25443/trf/user/runHPO-00-00-01'
 taskParamMap['processingType'] = 'simul'
 taskParamMap['prodSourceLabel'] = 'test'
 taskParamMap['taskType'] = 'prod'
@@ -25,8 +29,9 @@ taskParamMap['coreCount'] = 1
 taskParamMap['site'] = 'BNL_PROD_UCORE'
 taskParamMap['nucleus'] = 'CERN-PROD'
 taskParamMap['cloud'] = 'WORLD'
-#taskParamMap['mergeOutput'] = True
+
 logDatasetName = 'panda.jeditest.log.{0}'.format(uuid.uuid4())
+
 taskParamMap['log'] = {'dataset': logDatasetName,
                        'type':'template',
                        'param_type':'log',
@@ -34,17 +39,28 @@ taskParamMap['log'] = {'dataset': logDatasetName,
                        'destination':'type=DATADISK,s3=1',
                        'offset':1000,
                        'value':'{0}.${{SN}}.log.tgz'.format(logDatasetName)}
+
+taskParamMap['hpoRequestData'] = {'sandbox': None,
+                                  'method': 'bayesian',
+                                  'opt_space': {'A': (1, 4), 'B': (1, 10)},
+                                  'initial_points': [({'A': 1, 'B': 2}, 0.3), ({'A': 1, 'B': 3}, None)],
+                                  'max_points': 20,
+                                  'num_points_per_generation': 10}
+
 taskParamMap['jobParameters'] = [
     {'type':'constant',
-     'value': 'some args'
+     'value': '-o out.json -j "{0}"'.format(quote('<<<command to execute the container>>>'))
      },
-    {'type':'constant',
-     'value':'AMITag=p1462'
+    {'type': 'constant',
+     'value': '--writeInputToTxt IN_DATA:input.txt'
+     },
+    {'type': 'constant',
+     'value': '--inMap "{\'IN_DATA\': ${IN_DATA/T}}"'
      },
     {'type':'template',
      'param_type':'input',
-     'value':'ds=${IN_DATA}',
-     'dataset':'mc16_13TeV.398184.MGPy8EG_A14N23LO_SlepSlep_direct_150p0_130p0.simul.HITS.e7506_e5984_a875_tid18097791_00',
+     'value':'-i "${IN_DATA/T}"',
+     'dataset':'<<<training dataset name>>>',
      'attributes': 'nosplit,repeat',
      },
     ]
