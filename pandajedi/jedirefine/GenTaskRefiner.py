3301a93137f51d0b1dd0d6924eece43d22b2b275
tmaeno <tmaeno@bnl.gov>
2021-06-10 12:41:11 +0200

    for rubin

--- pandajedi/jedirefine/GenTaskRefiner.py
+++ pandajedi/jedirefine/GenTaskRefiner.py
@@ -1,6 +1,5 @@
 from .TaskRefinerBase import TaskRefinerBase
 
-
 # refiner for general purpose
 class GenTaskRefiner (TaskRefinerBase):
 
@@ -12,6 +11,8 @@ class GenTaskRefiner (TaskRefinerBase):
     def extractCommon(self, jediTaskID, taskParamMap, workQueueMapper, splitRule):
         if 'cloud' not in taskParamMap and 'workingGroup' in taskParamMap:
             taskParamMap['cloud'] = taskParamMap['workingGroup']
+        if 'transPath' not in taskParamMap:
+            taskParamMap['transPath'] = 'https://atlpan.web.cern.ch/atlpan/runGen-00-00-02'
         # update task parameters
         self.updatedTaskParams = taskParamMap
         # call base method
