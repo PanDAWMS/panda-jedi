51465a6ebc0ab4fff9d3bca845dfa08e36b79d1c
tmaeno <tmaeno@bnl.gov>
2021-07-13 11:57:04 +0200

    fixed rounding errors in ContentsFeeder

--- pandajedi/jediorder/ContentsFeeder.py
+++ pandajedi/jediorder/ContentsFeeder.py
@@ -322,14 +322,14 @@ class ContentsFeederThread (WorkerThread):
                                                 nPFN = origNumFiles
                                                 if 'nEventsPerJob' in taskParamMap and 'nEventsPerFile' in taskParamMap \
                                                         and taskParamMap['nEventsPerFile'] > taskParamMap['nEventsPerJob']:
-                                                    nPFN = nPFN * taskParamMap['nEventsPerFile'] // taskParamMap['nEventsPerJob']
+                                                    nPFN = nPFN * math.ceil(taskParamMap['nEventsPerFile'] / taskParamMap['nEventsPerJob'])
                                                 elif 'nEventsPerFile' in taskParamMap and 'nEventsPerRange' in taskParamMap:
                                                     nPFN = nPFN * taskParamMap['nEventsPerFile'] // taskParamMap['nEventsPerRange']
                                             elif 'nEvents' in taskParamMap and 'nEventsPerJob' in taskParamMap:
-                                                nPFN = taskParamMap['nEvents'] // taskParamMap['nEventsPerJob']
+                                                nPFN = math.ceil(taskParamMap['nEvents'] / taskParamMap['nEventsPerJob'])
                                             elif 'nEvents' in taskParamMap and 'nEventsPerFile' in taskParamMap \
                                                     and taskSpec.getNumFilesPerJob() is not None:
-                                                nPFN = taskParamMap['nEvents'] // taskParamMap['nEventsPerFile'] // taskSpec.getNumFilesPerJob()
+                                                nPFN = math.ceil(taskParamMap['nEvents'] / taskParamMap['nEventsPerFile'] / taskSpec.getNumFilesPerJob())
                                             else:
                                                 # the default number of records for seq_number
                                                 seqDefNumRecords = 10000
