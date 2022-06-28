0658a4f064c6a791fd990625665457d73ecc495d
fbarreir <fh.barreiro@gmail.com>
2022-06-28 17:28:53 +0200

    Moved production throttle to the base to use it in analysis

--- pandajedi/jedithrottle/AtlasAnalJobThrottler.py
+++ pandajedi/jedithrottle/AtlasAnalJobThrottler.py
@@ -4,26 +4,34 @@ from .JobThrottlerBase import JobThrottlerBase
 
 # logger
 from pandacommon.pandalogger.PandaLogger import PandaLogger
+
 logger = PandaLogger().getLogger(__name__.split('.')[-1])
 
 
 # class to throttle ATLAS analysis jobs
-class AtlasAnalJobThrottler (JobThrottlerBase):
+class AtlasAnalJobThrottler(JobThrottlerBase):
 
     # constructor
-    def __init__(self,taskBufferIF):
-        JobThrottlerBase.__init__(self,taskBufferIF)
-
+    def __init__(self, taskBufferIF):
+        self.comp_name = 'anal_job_throttler'
+        self.app = 'jedi'
+        JobThrottlerBase.__init__(self, taskBufferIF)
 
     # check if throttled
-    def toBeThrottled(self, vo, prodSourceLabel, cloudName, workQueue, resourceType):
+    def toBeThrottled(self, vo, prodSourceLabel, cloudName, workQueue, resource_name):
+        return self.toBeThrottledBase(vo, prodSourceLabel, cloudName, workQueue, resource_name)
+
+    # check if throttled
+    def toBeThrottled_old(self, vo, prodSourceLabel, cloudName, workQueue, resource_name):
         # make logger
         tmpLog = MsgWrapper(logger)
-        tmpLog.debug('start vo={0} label={1} cloud={2} workQueue={3}'.format(vo,prodSourceLabel,cloudName,
+        tmpLog.debug('start vo={0} label={1} cloud={2} workQueue={3}'.format(vo, prodSourceLabel, cloudName,
                                                                              workQueue.queue_name))
+
         # check if unthrottled
         if not workQueue.throttled:
             tmpLog.debug("  done : unthrottled since throttled is False")
             return self.retUnThrottled
+
         tmpLog.debug("  done : SKIP")
         return self.retThrottled
