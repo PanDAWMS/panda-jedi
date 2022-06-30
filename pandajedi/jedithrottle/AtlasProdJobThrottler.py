e8a0b1809ac31c9ce28badd54edec4bd8f759303
fbarreir <fh.barreiro@gmail.com>
2022-06-30 13:13:13 +0200

    moved constants

--- pandajedi/jedithrottle/AtlasProdJobThrottler.py
+++ pandajedi/jedithrottle/AtlasProdJobThrottler.py
@@ -5,15 +5,6 @@ from .JobThrottlerBase import JobThrottlerBase
 from pandacommon.pandalogger.PandaLogger import PandaLogger
 logger = PandaLogger().getLogger(__name__.split('.')[-1])
 
-LEVEL_None = 0 # There is no configuration defined
-LEVEL_GS = 1 # There is a configuration defined at global share level
-LEVEL_MS = 2 # There is a configuration defined at MCORE/SCORE level
-LEVEL_RT = 3 # There is a configuration defined at resource type level
-
-NQUEUELIMIT = 'NQUEUELIMIT'
-NRUNNINGCAP = 'NRUNNINGCAP'
-NQUEUECAP = 'NQUEUECAP'
-
 # workqueues that do not work at resource type level.
 # E.g. event service is a special case, since MCORE tasks generate SCORE jobs. Therefore we can't work at
 # resource type level and need to go to the global level, in order to avoid over-generating jobs
