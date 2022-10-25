f91ae2f0941913b454838e071a1450fedd3c56d4
Edward Karavakis <edward.karavakis@cern.ch>
2022-10-25 14:41:07 +0200

    ATLASPANDA-655 Added schema checker script for jedi

---
+++ pandajedi/jeditest/SchemaChecker.py
@@ -0,0 +1,36 @@
+"""
+checking DB schema version for PanDA JEDI
+
+"""
+
+from pandajedi.jediconfig import jedi_config
+from pandajedi.jedicore.JediDBProxy import DBProxy
+
+from pandajedi.jedicore import JediDBSchemaInfo
+
+#panda_config.dbhost = 'panda-postgres'
+proxyS = DBProxy()
+proxyS.connect(panda_config.dbhost, panda_config.dbpasswd, panda_config.dbuser, panda_config.dbname)
+
+sql = "select major || '.' || minor || '.' || patch from ATLAS_PANDA.pandadb_version where component = 'SCHEMA'"
+
+res = proxyS.querySQL(sql)
+dbVersion = res[0][0].split('.')
+
+# covert string to list of ints to compare numbers
+dbVersionIntegers = list(map(int, dbVersion))
+
+minimumSchema = JediDBSchemaInfo.JediDBSchemaInfo().method()
+serverDBVersion = minimumSchema.split('.')
+
+# covert string to list of ints to compare numbers
+serverDBVersionIntegers = list(map(int, serverDBVersion))
+
+if dbVersionIntegers[0] == serverDBVersionIntegers[0] and dbVersionIntegers[1] == serverDBVersionIntegers[1]:
+    if dbVersionIntegers[2] > serverDBVersionIntegers[2]:
+        print ("False")
+    else:
+        print ("True")
+else:
+    print ("False")
+
