8abafe05c6242ecf256e3941bfda1d36b5b8a21a
Edward Karavakis <edward.karavakis@cern.ch>
2022-11-07 16:15:13 +0100

    Sending an email if the pandaEmailNotification var is defined in the panda config file.

--- pandajedi/jeditest/SchemaChecker.py
+++ pandajedi/jeditest/SchemaChecker.py
@@ -1,10 +1,13 @@
 """
-checking DB schema version for PanDA JEDI
+Checking DB schema version for PanDA JEDI.
+If there is an issue and the pandaEmailNotification var is
+defined in panda_config, it will send an email notification.
 """
 
 from pandajedi.jediconfig import jedi_config
+from pandaserver.config import panda_config
 from pandajedi.jedicore.JediDBProxy import DBProxy
-
+from pandaserver.srvcore.MailUtils import MailUtils
 from pandajedi.jedicore import JediDBSchemaInfo
 
 from packaging import version
@@ -23,3 +26,9 @@ if version.parse(dbVersion) >= version.parse(serverDBVersion):
     return ("True")
 else:
     return ("False")
+    if 'pandaEmailNotification' in panda_config.__dict__:
+        msgBody = 'There is an issue with ' + panda_config.pserveralias + '. PanDA DB schema installed is ' + dbVersion + \
+                  ' while PanDA JEDI requires version ' + serverDBVersion + \
+                  ' to be installed. Please check the official docs for instructions on how to upgrade the schema.'
+        MailUtils().send(panda_config.pandaEmailNotification,
+                         'PanDA DB Version installed is not correct for JEDI running on ' + panda_config.pserveralias, msgBody)
