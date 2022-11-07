842f9a86f13067c412073425722219ae0c3dfa54
Edward Karavakis <edward.karavakis@cern.ch>
2022-11-07 14:26:21 +0100

    Changed the way we do the comparison after the discussion with Tadashi and Fernando

--- pandajedi/jeditest/SchemaChecker.py
+++ pandajedi/jeditest/SchemaChecker.py
@@ -7,28 +7,19 @@ from pandajedi.jedicore.JediDBProxy import DBProxy
 
 from pandajedi.jedicore import JediDBSchemaInfo
 
-#panda_config.dbhost = 'panda-postgres'
+from packaging import version
+
 proxyS = DBProxy()
 proxyS.connect(jedi_config.db.dbhost, jedi_config.db.dbpasswd, jedi_config.db.dbuser, jedi_config.db.dbname)
 
-sql = "select major || '.' || minor || '.' || patch from ATLAS_PANDA.pandadb_version where component = 'SCHEMA'"
+sql = "select major || '.' || minor || '.' || patch from ATLAS_PANDA.pandadb_version where component = 'JEDI'"
 
 res = proxyS.querySQL(sql)
-dbVersion = res[0][0].split('.')
+dbVersion = res[0][0]
 
-# covert string to list of ints to compare numbers
-dbVersionIntegers = list(map(int, dbVersion))
+serverDBVersion = JediDBSchemaInfo.JediDBSchemaInfo().method()
 
-minimumSchema = JediDBSchemaInfo.JediDBSchemaInfo().method()
-serverDBVersion = minimumSchema.split('.')
-
-# covert string to list of ints to compare numbers
-serverDBVersionIntegers = list(map(int, serverDBVersion))
-
-if dbVersionIntegers[0] == serverDBVersionIntegers[0] and dbVersionIntegers[1] == serverDBVersionIntegers[1]:
-    if dbVersionIntegers[2] > serverDBVersionIntegers[2]:
-        print ("False")
-    else:
-        print ("True")
+if version.parse(dbVersion) >= version.parse(serverDBVersion):
+    return ("True")
 else:
-    print ("False")
+    return ("False")
