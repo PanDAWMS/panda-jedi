ef69d7d609cc6392910f2c57a3e596d059c81861
mightqxc <fahui.lin@cern.ch>
2023-02-10 09:53:42 +0100

    v0.0.20

--- setup.py
+++ setup.py
@@ -230,7 +230,7 @@ setup(
               ],
     install_requires=[
         'six',
-        'panda-common',
+        'panda-common>=0.0.33',
         'panda-server>=0.0.37',
         'python-daemon',
         'numpy',
