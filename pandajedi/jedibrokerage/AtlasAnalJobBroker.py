a7130a8f96bd5b72b539daddd4b349acf323f401
mightqxc <fahui.lin@cern.ch>
2023-05-30 12:08:02 +0200

    analy broker: INFO logs about task and site class

--- pandajedi/jedibrokerage/AtlasAnalJobBroker.py
+++ pandajedi/jedibrokerage/AtlasAnalJobBroker.py
@@ -1899,6 +1899,7 @@ class AtlasAnalJobBroker(JobBrokerBase):
                     bw_map = basic_weight_compar_map[site]
                     prt_str = prt_str_temp.format(site=site, **{ k: (x if x is not None else math.nan) for k, x in bw_map.items() })
                     prt_str_list.append(prt_str)
+                tmpLog.info('gshare: {} ,{} task_class: {}'.format(taskSpec.gshare, (' merging,' if inputChunk.isMerging else ''), task_class_value))
                 tmpLog.debug('WEIGHT-COMPAR: for gshare={},{} cl={}, nJobsEst={} got \n{}'.format(
                                 taskSpec.gshare, (' merging,' if inputChunk.isMerging else ''), task_class_value, n_jobs_to_submit, '\n'.join(prt_str_list)))
         except Exception as e:
@@ -1945,7 +1946,9 @@ class AtlasAnalJobBroker(JobBrokerBase):
             siteCandidateSpec.override_attribute('maxwdir', newMaxwdir.get(tmpSiteName))
             # set weight
             siteCandidateSpec.weight = weight
-            tmpStr = 'weight={0:.7f} ver={1} '.format(weight, _basic_weight_version)
+            tmpStr = 'weight={0:.7f} '.format(weight)
+            tmpStr += 'class={0} trr={1:.3f} '.format(bw_map['class'], bw_map['trr'])
+            tmpStr += 'userQ={0} userQRem={1:.3f} '.format(bw_map['user_q_len'], bw_map['rem_q_len'])
             tmpStr += 'nRunning={0} nDefined={1} nActivated={2} nStarting={3} nAssigned={4} '.format(   bw_map['nr'],
                                                                                                         bw_map['nDefined'],
                                                                                                         bw_map['nActivated'],
