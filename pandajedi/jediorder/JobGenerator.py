5c6379477fb8139f374de7e3f92af215fa81e0e1
Fernando Harald Barreiro Megino <fh.barreiro@gmail.com>
2024-03-19 12:36:14 +0100

    Debug info

--- pandajedi/jediorder/JobGenerator.py
+++ pandajedi/jediorder/JobGenerator.py
@@ -121,7 +121,8 @@ class JobGenerator(JediKnight):
                                     active = workQueue.queue_name in active_gshare_rtypes and resource_type in active_gshare_rtypes[workQueue.queue_name]
                                     tmpLog_inner.debug(f"active_gshare_rtypes: {active_gshare_rtypes}")
                                     tmpLog_inner.debug(f"workQueue.queue_name: {workQueue.queue_name}")
-                                    tmpLog_inner.debug(f"active: {active}")
+                                    tmpLog_inner.debug(f"queue present: {workQueue.queue_name in active_gshare_rtypes}")
+                                    tmpLog_inner.debug(f"rtype present: {resource_type in active_gshare_rtypes[workQueue.queue_name]}")
                                     if active_gshare_rtypes and not active:
                                         if random.uniform(0, 1) > inactive_poll_probability:
                                             tmpLog_inner.debug(f"skipping {cycleStr} due to inactivity")
