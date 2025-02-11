05d94c47e847a7d5fe80f9febb466c001fd53d94
mightqxc <mightqxc@hotmail.com>
2025-02-11 15:47:34 +0100

    pretty

--- pandajedi/jediorder/TaskRefiner.py
+++ pandajedi/jediorder/TaskRefiner.py
@@ -152,11 +152,14 @@ class TaskRefinerThread(WorkerThread):
                             tmpLog.error(errStr)
                             tmpStat = Interaction.SC_FAILED
                     # get data carousel config map
-                    dc_config_map = data_carousel_interface.dc_config_map
-                    # enable input pre-staging for early access user
+                    if tmpStat == Interaction.SC_SUCCEEDED:
+                        dc_config_map = data_carousel_interface.dc_config_map
+                    # adjust task parameters
                     if "inputPreStaging" not in taskParamMap:
-                        if dc_config_map and taskParamMap.get("userName") in dc_config_map.early_access_users:
+                        if dc_config_map and (user_name := taskParamMap.get("userName")) in dc_config_map.early_access_users:
+                            # enable input pre-staging for early access user
                             taskParamMap["inputPreStaging"] = True
+                            tmpLog.debug(f"set inputPreStaging for data carousel early access user {user_name}")
                     # extract common parameters
                     if tmpStat == Interaction.SC_SUCCEEDED:
                         tmpLog.info("extracting common")
