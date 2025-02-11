d0cc4b709ec7fa01a3a838c71c536553ae0fc5ec
mightqxc <mightqxc@hotmail.com>
2025-02-11 14:51:49 +0100

    data carousel: fix to propagate inputPreStaging to taskspec

--- pandajedi/jediorder/TaskRefiner.py
+++ pandajedi/jediorder/TaskRefiner.py
@@ -151,6 +151,12 @@ class TaskRefinerThread(WorkerThread):
                             errStr = f"failed to get task refiner with {errtype.__name__}:{errvalue}"
                             tmpLog.error(errStr)
                             tmpStat = Interaction.SC_FAILED
+                    # get data carousel config map
+                    dc_config_map = data_carousel_interface.dc_config_map
+                    # enable input pre-staging for early access user
+                    if "inputPreStaging" not in taskParamMap:
+                        if dc_config_map and taskParamMap.get("userName") in dc_config_map.early_access_users:
+                            taskParamMap["inputPreStaging"] = True
                     # extract common parameters
                     if tmpStat == Interaction.SC_SUCCEEDED:
                         tmpLog.info("extracting common")
@@ -194,12 +200,6 @@ class TaskRefinerThread(WorkerThread):
                             tmpStat = Interaction.SC_FAILED
                     # data carousel (input pre-staging) ; currently only for analysis tasks
                     if tmpStat == Interaction.SC_SUCCEEDED:
-                        # get data carousel config map
-                        dc_config_map = data_carousel_interface.dc_config_map
-                        # enable input pre-staging for early access user
-                        if "inputPreStaging" not in taskParamMap:
-                            if dc_config_map and taskParamMap.get("userName") in dc_config_map.early_access_users:
-                                taskParamMap["inputPreStaging"] = True
                         # check datasets to pre-stage
                         if taskParamMap.get("inputPreStaging") and taskParamMap.get("taskType") == "anal" and taskParamMap.get("prodSourceLabel") == "user":
                             tmpLog.info("checking about data carousel")
